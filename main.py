9a988a3888b5bb11a58b0e2782e8fd32bd7430a2
Adam <adam.venroy@gmail.com>
2023-12-28 01:24:36 +1300

    Warnings no longer appear now, changed sentiment column to target and integers instead of strings.

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ DATASET_FILE_NAME = "dataset.zip"
 MAXIMUM_SEQ_LENGTH = 10000 # Number of maximum tokens
 MAXIMUM_INPUT_POINTS = 250 # Number of perceptrons on input layer
 
+
 def get_dataset_contents(file_name) -> pd.DataFrame:
     """ Returns pandas dataframe of IMDB reviews and sentiment. """
     return pd.read_csv(file_name)
@@ -21,17 +22,20 @@ def standardize_data(df: pd.DataFrame) -> pd.DataFrame:
     df["review"] = df["review"].apply(lambda x: re.sub(r'[^\w\s]', '', x))
     return df
 
+
 def vectorize_data(df: pd.DataFrame) -> pd.DataFrame:
     """ Given a standardized dataframe, returns a vectorized dataframe """
-    #TODO: Get rid of error messages
-    df = df[df["review"].map(len) <= MAXIMUM_SEQ_LENGTH]
+    df = df[df["review"].map(lambda x: len(x.split())) <= MAXIMUM_SEQ_LENGTH]
     input_tokens = [tup[0] for tup in Counter(" ".join(df["review"]).split()).most_common(MAXIMUM_INPUT_POINTS)]
-    vectorized_df = pd.DataFrame()
-    for token in input_tokens:
-        vectorized_df[token] = df["review"].apply(lambda x: int(token in x))
-    vectorized_df["sentiment"] = df["sentiment"]
-    return vectorized_df    
+    series_list = []
 
+    for token in input_tokens:
+        series_list.append(df["review"].apply(lambda x: int(token in x)))
+
+    vectorized_df = pd.concat(series_list, axis=1, keys=input_tokens)
+    vectorized_df['target'] = df['sentiment'].str.contains('positive')
+    vectorized_df['target'] = vectorized_df['target'].apply(int)
+    return vectorized_df
 
 
 def classify_text() -> list:
@@ -50,3 +54,4 @@ if __name__ == "__main__":
     x = get_dataset_contents(DATASET_FILE_NAME)
     x = standardize_data(x)
     x = vectorize_data(x)
+    print(x)
